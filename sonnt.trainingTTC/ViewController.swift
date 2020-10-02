2a906055c2e2575c459bafe6316583e152ac3c38
tuanson <sonnt@ttc-solutions@gmail.com.vn>
2020-10-02 08:21:41 +0700

    Initial Commit

---
+++ sonnt.trainingTTC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  sonnt.trainingTTC
+//
+//  Created by Nero on 10/2/20.
+//  Copyright © 2020 Nero. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
